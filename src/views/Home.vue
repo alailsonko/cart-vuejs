e0a3b996920765048e0657a7179302687dd8ec5c
alailsonko <aalailson3@gmail.com>
2021-07-05 09:32:41 -0300

    insert components in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="home">
     <Categories msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Products msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Categories from '@/components/Categories.vue'; // @ is an alias to /src
+import Products from '@/components/Products.vue'; // @ is an alias to /src
 
 @Options({
   components: {
     Categories,
+    Products,
   },
 })
 export default class Home extends Vue {}
 </script>
 
 <style scoped lang="scss">
+.home {
+   display: flex;
 
+}
 </style>
